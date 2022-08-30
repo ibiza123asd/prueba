367f79c3cbbe31ff8d40914ef31504a9c106c1b9
unknown <guz_of_14@hotmail.com>
2022-08-30 17:54:38 -0500

    primer commit

---
+++ src/conexion2/Conexion2.java
@@ -0,0 +1,20 @@
+/*
+
+
+ */
+package conexion2;
+
+/*/**
+*
+* @author Brandon
+*/
+public class Conexion2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
